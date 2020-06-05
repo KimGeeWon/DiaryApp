2a9da4c38cb06d69083665966afb097c340565b3
KimGeeWon <kjw403300@gmail.com>
2020-06-05 09:22:03 +0900

    [ADD] dababase CRUD
    crud 생성

--- DiaryApp/app/src/main/java/com/example/mydiary/Databases.java
+++ DiaryApp/app/src/main/java/com/example/mydiary/Databases.java
@@ -2,9 +2,10 @@ package com.example.mydiary;
 
 import android.provider.BaseColumns;
 
+import java.util.Date;
+
 public class Databases {
     public static final class CreateDB implements BaseColumns {
-        public static final String ID = "id";
         public static final String TITLE = "title";
         public static final String CONTENT = "content";
         public static final String CREATED = "created";
@@ -12,10 +13,10 @@ public class Databases {
         public static final String TABLENAME = "Diary";
 
         public static final String CREATE = "create table if not exists "+TABLENAME+"("
-                +ID+" INTEGER primary key autoincrement, "
-                +TITLE+" TEXT not null , "
-                +CONTENT+" TEXT not null , "
-                +CREATED+" DATE not null , "
-                +MODIFIED+" DATE );";
+                + _ID + " INTEGER primary key autoincrement, "
+                + TITLE + " TEXT not null , "
+                + CONTENT + " TEXT not null , "
+                + CREATED + " DATETIME DEFAULT (datetime('now', 'localtime')) not null , "
+                + MODIFIED + " DATETIME );";
     }
 }
--- DiaryApp/app/src/main/java/com/example/mydiary/DbOpenHelper.java
+++ DiaryApp/app/src/main/java/com/example/mydiary/DbOpenHelper.java
@@ -1,9 +1,13 @@
 package com.example.mydiary;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Date;
 
 public class DbOpenHelper {
 
@@ -47,5 +51,46 @@ public class DbOpenHelper {
 
     public void close(){
         mDB.close();
+    }
+
+    public long insertColumn(String title, String content, Date created, Date modified){
+        ContentValues values = new ContentValues();
+
+        values.put(Databases.CreateDB.TITLE, title);
+        values.put(Databases.CreateDB.CONTENT, content);
+        values.put(Databases.CreateDB.CREATED, String.valueOf(created));
+        values.put(Databases.CreateDB.MODIFIED, String.valueOf(modified));
+        return mDB.insert(Databases.CreateDB.TABLENAME, null, values);
+    }
+
+    // Update DB
+    public boolean updateColumn(long id, String title, String content, Date created, Date modified){
+        ContentValues values = new ContentValues();
+        values.put(Databases.CreateDB.TITLE, title);
+        values.put(Databases.CreateDB.CONTENT, content);
+        values.put(Databases.CreateDB.CREATED, String.valueOf(created));
+        values.put(Databases.CreateDB.MODIFIED, String.valueOf(modified));
+        return mDB.update(Databases.CreateDB.TABLENAME, values, "_id=" + id, null) > 0;
+    }
+
+    // Delete All
+    public void deleteAllColumns() {
+        mDB.delete(Databases.CreateDB.TABLENAME, null, null);
+    }
+
+    // Delete DB
+    public boolean deleteColumn(long id){
+        return mDB.delete(Databases.CreateDB.TABLENAME, "_id="+id, null) > 0;
+    }
+
+    // Select DB
+    public Cursor selectColumns(){
+        return mDB.query(Databases.CreateDB.TABLENAME, null, null, null, null, null, null);
+    }
+
+    // sort by column
+    public Cursor sortColumn(String sort){
+        Cursor c = mDB.rawQuery( "SELECT * FROM Diary ORDER BY " + sort + ";", null);
+        return c;
     }
 }
